0fac5c378f264eb9f84c5ba69b7865a0b407de40
Jorge <jorge.toledo.6@hotmail.com>
2022-08-30 18:47:01 -0400

    Ejercicio 1

---
+++ Ejercicios/Ejercicio1/Model.py
@@ -0,0 +1,4 @@
+#Identificar y generar los modelos de clases para crear un sistema que administre un taller mecanico.
+#Es decir, las reparaciones que en este se generan, los repuestos que se utilizan, los autos que ingresan 
+#y los clientes a los que se atienden.
+
