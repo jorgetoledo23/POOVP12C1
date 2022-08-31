1025dbb8929ce5be555b42147db8306d6d0f9dad
Jorge <jorge.toledo.6@hotmail.com>
2022-08-30 20:19:15 -0400

    Ejercicios Resueltos

--- Ejercicios/Ejercicio1/Model.py
+++ Ejercicios/Ejercicio1/Model.py
@@ -2,6 +2,50 @@
 #Es decir, las reparaciones que en este se generan, los repuestos que se utilizan, los autos que ingresan 
 #los mecanicos que hacen las reparaciones y los clientes a los que se atienden.
 
+class Reparacion:
+    fecha = ""
+    codigo = ""
+    cliente = ""
+    mecanico = ""
+    auto = ""
+    observacion = ""
+    repuestos = ""
+    valor = ""
+
+class Cliente:
+    rut = ""
+    nombres =""
+    apellidos =""
+    direccion =""
+    comuna =""
+    telefono = ""
+    correo =""
+
+class Mecanico:
+    rut = ""
+    nombres =""
+    apellidos =""
+    direccion =""
+    comuna =""
+    telefono = ""
+    correo =""
+
+class Auto:
+    patente =""
+    modelo =""
+    marca =""
+    vim = ""
+    color = ""
+    año = ""
+    dueño = ""
+
+class Repuesto:
+    codigo = ""
+    nombre =""
+    marca =""
+    valor =""
+
+
 
 
 
