96fe9af803b741cc44443475336832b759f06061
Jorge <jorge.toledo.6@hotmail.com>
2022-09-06 20:43:47 -0300

    Ejercicio 3

--- Ejercicios/Ejercicio3/Ejercicio 3.py
+++ Ejercicios/Ejercicio3/Ejercicio 3.py
@@ -1,1 +1,22 @@
-#Crear una class Auto y Cliente, dentro de los 
+from msilib.schema import Class
+
+
+class Dueno:
+    Rut = ""
+    Nombre = ""
+    Apellidos = ""
+
+
+class Auto:
+    Patente = ""
+    Marca = ""
+    Modelo = ""
+    Año = ""
+    Color = ""
+    Dueño = ""
+
+
+#A las dos clases anteriores:
+#1-Generar sus respectivos constructores solicitando todos los atributos de cada class
+#2-Validar en la clase cliente que Rut, Nombre y Apellido sean obligatoriamente de tipo cadena de texto (str)
+#3-Validar en la clase Auto que el atributo dueño sea obligatoriamente de tipo Dueno
