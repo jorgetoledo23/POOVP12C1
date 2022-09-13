c4369d81417ba303676a5d6d2df89e2a3c2c38e1
Jorge <jorge.toledo.6@hotmail.com>
2022-09-13 18:39:53 -0300

    Mini Game

--- Ejercicios/Ejercicio2/Model.py
+++
@@ -1,7 +0,0 @@
-#Identificar y generar los modelos de clases para crear un sistema para administrar las atenciones medicas
-#de una consulta.
-#Es decir, controlar los medicos que trabajan, las horas medicas, a que pacientes atienden, y el detalle de cada consulta
-#que realizan.
-
-
-
--- Model/PrendaVestir.py
+++
@@ -1,8 +0,0 @@
-class Prenda:
-    Marca = ""
-    Talla = ""
-    Color = ""
-    Tipo = ""
-
-    def getInfo(self):
-        return f"{self.Tipo} Talla: {self.Talla}, Color: {self.Color}"
